8a65bcf084fdbf1a002fccbaedff2d92b65332b5
caleb1314 <key546273@gmail.com>
2026-07-17 00:47:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
 const CACHE_NAME = 'liquid-desktop-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
